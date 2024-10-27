ef06e186c1a86b174d5efcb35bc0ab8a54e23e25
Infi <infi@infi.sh>
2024-10-27 02:40:15 +0200

    feat: add realtime service (unused for now)
    Signed-off-by: Infi <infi@infi.sh>

---
+++ app/src/main/java/chat/revolt/api/realtime/RealtimeService.kt
@@ -0,0 +1,29 @@
+package chat.revolt.api.realtime
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+
+
+class RealtimeService : Service() {
+    class LocalBinder : Binder() {
+        fun getService(): RealtimeService {
+            return RealtimeService()
+        }
+    }
+
+    private val binder = LocalBinder()
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY;
+    }
+
+    override fun onCreate() {
+
+    }
+}
