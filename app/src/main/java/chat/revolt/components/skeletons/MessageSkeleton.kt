d5d3d13ee55dfdda521c64bae598f3c82bbd7b93
Infi <infi@infi.sh>
2024-11-09 23:11:24 +0100

    feat: add previews for message skeletons
    Signed-off-by: Infi <infi@infi.sh>

--- app/src/main/java/chat/revolt/components/skeletons/MessageSkeleton.kt
+++ app/src/main/java/chat/revolt/components/skeletons/MessageSkeleton.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import chat.revolt.api.settings.LoadedSettings
 
@@ -81,4 +82,22 @@ fun MessageSkeleton(variant: MessageSkeletonVariant, modifier: Modifier = Modifi
             )
         }
     }
+}
+
+@Preview
+@Composable
+private fun MessageSkeletonOnePreview() {
+    MessageSkeleton(MessageSkeletonVariant.One)
+}
+
+@Preview
+@Composable
+private fun MessageSkeletonTwoPreview() {
+    MessageSkeleton(MessageSkeletonVariant.Two)
+}
+
+@Preview
+@Composable
+private fun MessageSkeletonThreePreview() {
+    MessageSkeleton(MessageSkeletonVariant.Three)
 }
